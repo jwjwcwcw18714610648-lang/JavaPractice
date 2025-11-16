8e7e3f83c8b07e2b546f8241030d54eb376a01d1
JwCw <jwjwcwcw18714610648@gmail.com>
2025-11-16 23:29:49 +0800

    引入单元测试

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,3 +1,4 @@
+import org.junit.Test;
 import sun.net.util.ProxyUtil;
 
 public class test {
@@ -12,4 +13,15 @@ public class test {
     bigStar2.sing("逆战");
     bigStar2.dance("舞动青春");
     }
+    @Test
+    public void testProxy(){
+        BigStar bigStar=new BigStar("cxk");
+        StarCommen starCommen= new JDkProxyFactory().CreateProxy(bigStar);
+        starCommen.sing("东风破");
+        starCommen.dance("天鹅湖");
+        BigStar bigStar2= (BigStar) new CGLIBFactory().getProxyCGLIB(BigStar.class);
+        bigStar2.sing("逆战");
+        bigStar2.dance("舞动青春");
+    }
+
 }
